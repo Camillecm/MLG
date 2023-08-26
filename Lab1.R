9eb76ac862a91f507d043718c0e86f7c7e8ac8ba
Camille Menezes Pereira dos Santos <camillemenezes1504@gmail.com>
2023-08-26 13:55:10 -0300

    Atualizacao

--- Lab1.R
+++ Lab1.R
@@ -2,13 +2,45 @@ library(dplyr)
 library(tidyverse)
 library(faraway)
 
+# proporção da dose
+#doseamt quantidade da dose<-fator
+#taxa da dose dose aplicada
+#número de cromossomas anormais observados
+#cells é o número de células (dentre 1000)
+#ca número de anormalidade
+
 data(dicentric, package="faraway")
-summary(dicentric)
+dicentric <- dicentric|>
+  mutate(doseamt = factor(doseamt))
 glimpse(dicentric)
 rate<-dicentric$ca/dicentric$cells 
 
 #analise descritiva
+summary(dicentric)
 
+ggplot(dicentric)+
+  aes(x=ca)+
+  geom_histogram(bins = 15,color = "gray", fill = "black")+
+  labs(x="Número de anormalidades", y = "Frequência")+
+  theme_bw()
 
+ggplot(dicentric)+
+  aes(x=rate)+
+  geom_histogram(bins = 15,color = "gray", fill = "black")+
+  labs(x = "Taxa de anormalidades", y = "Frequência")+
+  theme_bw()
 
+ggplot()+
+  geom_boxplot(aes(y = rate))+
+  scale_x_discrete(breaks = NULL)+
+  labs(y = "Taxa de anormalidades")+
+  theme_bw()
 
+ggplot(dicentric)+
+  geom_boxplot(aes(x=doseamt, y=rate))+
+  labs(x = "Quantidade da dose", y = "Taxa de anormalidades")+
+  theme_bw()
+
+with(dicentric, interaction.plot(doseamt, doserate, ca/cells, legend=TRUE))
+
+#modelagem

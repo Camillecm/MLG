a7f743aeb52a1f10088bfbeed321ce729f25562b
Camille Menezes Pereira dos Santos <camillemenezes1504@gmail.com>
2023-08-26 13:24:01 -0300

    Atualizacao

--- Lab1.R
+++ Lab1.R
@@ -1,4 +1,14 @@
-library(readxl)
+library(dplyr)
 library(tidyverse)
+library(faraway)
+
+data(dicentric, package="faraway")
+summary(dicentric)
+glimpse(dicentric)
+rate<-dicentric$ca/dicentric$cells 
+
+#analise descritiva
+
+
 
 
